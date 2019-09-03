3754ff7c3d0d4e3e00d7c9eabb50b57a6588fffa
sundhar010 <sundhar.sai@springml.com>
2019-09-03 21:41:23 +0530

    adds selection sort support

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -12,34 +12,56 @@ export class DashboardComponent implements OnInit {
   constructor() {}
   randomArray = (length, max) =>
     [...new Array(length)].map(() => Math.round(Math.random() * max))
-  myData: Array[] = this.randomArray(100, 1000);
+  myData: Number[] = this.randomArray(30, 1000);
   ngOnInit() {
-    let i = 0,howManyTimes = 100;
-    let parent = this;
-    let f = () => {
-      parent.createChart('chart', parent.myData, i);
-      i++;
-      if (i < howManyTimes) {
-        setTimeout(f, 500);
-      }
-    };
-    f();
+    // let i = 0,howManyTimes = 100;
+    // let parent = this;
+    this.selectionSort();
+    // let f = () => {
+    //   parent.createChart('chart', parent.myData, i);
+    //   i++;
+    //   if (i < howManyTimes) {
+    //     setTimeout(f, 500);
+    //   }
+    // };
+    // f();
   }
-  createChart(id, data, index = -1) {
+  async selectionSort(){
+    let wait = ms => new Promise((r, j)=>setTimeout(r, ms))
+    for(let i = 0; i < this.myData.length; i++) {
+      let minIndex = i;
+      let min = this.myData[i];
+      for(let j = i; j < this.myData.length; j++) {
+        if(this.myData[j] <= min) {
+          min = this.myData[j];
+          minIndex = j;
+        }
+        this.createChart('chart', this.myData, i, j);
+        await wait(100);
+      }
+      this.createChart('chart', this.myData, i, minIndex);
+      await wait(500);
+      this.myData[minIndex] = this.myData[i];
+      this.myData[i] = min;
+      this.createChart('chart', this.myData, i, minIndex);
+      await wait(200);
+
+
+    }
+  }
+  createChart(id, data, indexI = -1, indexJ = -1) {
     const margin = { top: 20, right: 10, bottom: 20, left: 10 };
     const height = 500 - margin.top - margin.bottom;
     const width = 1250 - margin.left - margin.right;
     const yscale = d3
       .scaleLinear()
-      .domain([0, d3.max(data)])
+      .domain([0, +d3.max(data)])
       .range([0, height]);
     const xscale = d3
       .scaleBand()
       .domain(d3.range(data.length).map(d => d + ''))
       .rangeRound([0, width])
       .paddingInner(0.1);
-    console.log(xscale(600 + ''));
-    console.log(data);
     d3.select('#' + id).selectAll('*').remove();
     d3.select('#' + id)
       .append('svg')
@@ -53,20 +75,23 @@ export class DashboardComponent implements OnInit {
       .enter()
       .append('rect')
       .style('fill', (d, i) => {
-        if (i === index) {
+        if (i === indexI) {
           return 'red';
+        }
+        else if( i=== indexJ) {
+          return 'lightgreen';
         }
         return 'rgb(33, 150, 243)';
       })
       .attr('width', xscale.bandwidth())
       .attr('height', function(d) {
-        return yscale(d);
+        return yscale(+d);
       })
       .attr('x', function(d, i) {
         return xscale(i + '');
       })
       .attr('y', function(d) {
-        return height - yscale(d);
+        return height - yscale(+d);
       });
   }
 }
